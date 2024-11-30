fd80a876fdd14dcced8d9c7a24440774226c93fa
gapirajat <rajatshinde01@gmail.com>
2024-11-30 14:24:59 +0530

    announcement null safety

--- src/generalSlice/generalSlice.jsx
+++ src/generalSlice/generalSlice.jsx
@@ -56,7 +56,7 @@ const generalSlice = createSlice({
       state.isVisible = false;
     },
     setAnnouncement: (state, action) => {
-      const { announcement, link } = action.payload[0];
+      const { announcement, link } = action.payload?.[0] ?? null;;
       // console.log("payload");
       state.message = announcement;
       state.link = link || null;
@@ -73,9 +73,9 @@ const generalSlice = createSlice({
       .addCase(fetchAnnouncement.fulfilled, (state, action) => {
         // console.log(action.payload[0].announcement);
         state.loading = false;
-        state.message = action.payload[0].announcement;
+        state.message = action.payload?.[0]?.announcement ?? null;
         state.link = action.payload.link || null;//not implemtned
-        state.isVisible = shouldBeVisible(action.payload[0].announcement);
+        state.isVisible = shouldBeVisible(action.payload?.[0]?.announcement ?? null;);
       })
       .addCase(fetchAnnouncement.rejected, (state, action) => {
         state.loading = false;
